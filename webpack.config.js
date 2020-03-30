f2d09250985d5098869bbbb82a75a915c6dc2493
NLNT <crundee54321@gmail.com>
2020-03-30 17:40:58 +0700

    Added sound notification & fixed a bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path') // npm install path
 
 module.exports = {
-  mode: "development", // "production", "development", "none"
+  mode: "production", // "production", "development", "none"
   entry: './public/js/index.js', // path to your controller js file
   output: {
     path: path.resolve(__dirname, 'public'), // bundle file location (project/public/bundle.js) 
